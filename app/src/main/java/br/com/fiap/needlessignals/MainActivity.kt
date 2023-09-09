f90c5d39caa1cc55be97e9be942f0384f1f98366
Mathews Evangelista <mathews.evangelista@yaman.com.br>
2023-09-09 13:02:25 -0300

    First commit

---
+++ app/src/main/java/br/com/fiap/needlessignals/MainActivity.kt
@@ -0,0 +1,28 @@
+package br.com.fiap.needlessignals
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import br.com.fiap.needlessignals.ui.theme.NeedlesSignalsTheme
+import br.com.fiap.needlessignals.screens.Login
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NeedlesSignalsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Login()
+                }
+            }
+        }
+    }
+}
