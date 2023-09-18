c587d13e5f7b9f05553d4b4bb65d2f0c13417d41
Eder Sena <eder.sena@live.com>
2023-09-18 13:58:53 -0300

    Card E login

--- app/src/main/java/br/com/fiap/needlessignals/presentation/components/AtivoCard.kt
+++ app/src/main/java/br/com/fiap/needlessignals/presentation/components/AtivoCard.kt
@@ -9,16 +9,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Brightness1
 import androidx.compose.material.icons.outlined.StarBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,7 +28,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -40,56 +41,88 @@ import br.com.fiap.needlessignals.data.model.Ativos
 fun AtivoCard(
     ativo: Ativos,
 ) {
-    Card(
-        modifier = Modifier.run {
-            fillMaxWidth()
-                .padding(8.dp)
-                .clip(RoundedCornerShape(16.dp))
-                .clickable(onClick = {})
-        },
-        elevation = CardDefaults.cardElevation(16.dp),
-        colors = CardDefaults.cardColors(containerColor = Color.White)
+    Column {
+        Card(
+            modifier = Modifier.run {
+                fillMaxWidth()
+                    .padding(8.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .clickable(onClick = {})
+            },
+            elevation = CardDefaults.cardElevation(16.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.White)
 
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Image(
-                    modifier = Modifier
-                        .size(40.dp, 40.dp),
-                    imageVector = Icons.Outlined.StarBorder,
-                    contentDescription = "",
-                    contentScale = ContentScale.Crop,
-                    alignment = Alignment.CenterStart
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Image(
-                    modifier = Modifier
-                        .size(30.dp, 30.dp),
-                    painter = painterResource(id = R.drawable.eurusd),
-                    contentDescription = "",
-                    alignment = Alignment.CenterStart
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Column(
-                    modifier = Modifier.align(Alignment.CenterVertically)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
                 ) {
-                    Text(
-                        text = ativo.name,
-                        color = Color.Black,
-                        fontWeight = FontWeight.Bold,
+                    Image(
+                        modifier = Modifier
+                            .size(40.dp, 40.dp),
+                        imageVector = Icons.Outlined.StarBorder,
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        alignment = Alignment.CenterStart
                     )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Image(
+                        modifier = Modifier
+                            .size(30.dp, 30.dp),
+                        painter = painterResource(id = R.drawable.eurusd),
+                        contentDescription = "",
+                        alignment = Alignment.CenterStart
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Column(
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    ) {
+                        Text(
+                            text = ativo.name,
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                        )
 
+                    }
+                }
+                Row(
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(15.dp),
+                        imageVector = Icons.Filled.Brightness1,
+                        contentDescription = "",
+                        tint = if (ativo.isBuy) Color.Green else Color.Red
+                    )
+                    Icon(
+                        modifier = Modifier
+                            .size(15.dp),
+                        imageVector = Icons.Filled.Brightness1,
+                        contentDescription = "",
+                        tint = if (ativo.isBuy) Color.Green else Color.Red
+                    )
+                    Icon(
+                        modifier = Modifier
+                            .size(15.dp),
+                        imageVector = Icons.Filled.Brightness1,
+                        contentDescription = "",
+                        tint = if (ativo.isBuy) Color.Green else Color.Red
+                    )
                 }
             }
         }
+        Text(
+            modifier = Modifier.align(Alignment.End).padding(end=16.dp).offset(y = (-7).dp),
+            text = "Comprar com precaução"
+        )
     }
 
     @Composable
